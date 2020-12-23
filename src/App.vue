5dd384a24fbbd3d60d7e13f93c52e4c8665dd00e
Nicholas Moen <arcanemachine@tutanota.com>
2020-12-23 16:14:31 -0700

    Add component: PageFooter

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,18 @@
       <router-view id="content" :key="$route.path" class="mt-3"></router-view>
     </div>
 
+    <page-footer></page-footer>
+
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import PageFooter from './components/PageFooter.vue'
 
 export default {
   name: 'App',
-  components: { NavBar }
+  components: { NavBar, PageFooter }
 }
 </script>
 
